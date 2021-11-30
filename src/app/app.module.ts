ff2983feef9f4461dd89579456eed52976d1608b
Juanma Vega <juanmavega83@gmail.com>
2021-11-30 11:06:29 +0100

    create, close & passing data in modal component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CreateBookingComponent } from './bookings/create-booking/create-booking.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CreateBookingComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
